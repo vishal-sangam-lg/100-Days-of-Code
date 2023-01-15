d4911708edbf17686741df959a86dcefdc4e3d9a
Vishal Sangam L G <vishalsangamlg@Vishals-MacBook-Pro.local>
2023-01-15 22:42:49 +0530

    Day 1 🚀

---
+++ Day-1/band-name-generator.py
@@ -0,0 +1,5 @@
+# Project 1 - Band Name Generator
+print("Welcome to the Band Name Generator.")
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your band name could be " + city_name + " " + pet_name)
---
+++ Day-1/printing.py
@@ -0,0 +1,45 @@
+# Problem 1
+print("Problem 1")
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')\n")
+
+# Problem 2
+print("Problem 2")
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.\n")
+
+# Problem 3
+print("Problem 3")
+name = input("What is your name?")
+print(len(name))
+print()
+
+# Problem 4
+print("Problem 4")
+print("Hello " + input("What is your name?\n"))
+print()
+
+# Problem 5
+print("Problem 5")
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+
+temp = a
+a = b
+b = temp
+
+# Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+print()
