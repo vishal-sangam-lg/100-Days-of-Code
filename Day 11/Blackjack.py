5f008d6a06a4f27da605c61a305c71eb453eb6bd
Vishal Sangam L G <vishalsangamlg@Vishals-MacBook-Pro.local>
2023-01-23 23:21:32 +0530

    Day 11 🚀

--- Day 11/Blackjack.py
+++ Day 11/Blackjack.py
@@ -153,6 +153,6 @@ def play_game():
 
 # Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game
 # of blackjack and show the logo from art.py.
-while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+while input("Do you want to play a Game of Blackjack? Type 'y' or 'n': ") == "y":
     screen_clear()
     play_game()
