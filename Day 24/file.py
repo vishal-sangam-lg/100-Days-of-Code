1889ce2b07887ac9c154282df9d77e8ea7e394ca
Vishal Sangam L G <vishalsangamlg@Vishals-MacBook-Pro.local>
2023-02-05 16:08:50 +0530

    Day 24 🚀

--- Day 24/file.py
+++ Day 24/file.py
@@ -11,4 +11,4 @@ with open("my_file.txt", mode="a") as file:
     file.write("\nNew text.")
 
 with open("new_file.text", mode="w") as file:
-    file.write("\nNew text inside new_file.")
+    file.write("\nNew text inside new_file.txt")
